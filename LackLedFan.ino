4c900abd90a59f8e80e74340891d3f29897d5d6d
Nim <WemansG90@users.noreply.github.com>
2021-02-24 01:00:39 +0100

    Trimmed down to one encoder and just the PWM code

--- LackLedFan.ino
+++ LackLedFan.ino
@@ -1,44 +1,27 @@
-#include <FastLED.h>
 #include <CommonBusEncoders.h>
-
-//FastLED
-#define DATA_PIN        1
-#define LED_TYPE        WS2813
-#define NUM_LEDS        31
-CRGB leds[NUM_LEDS];
-#define COLOR_ORDER GRB
 
 //Fan PWM
 #define PWM6            OCR4D
 #define PWM6_MAX        OCR4C
 
 //Encoders
-CommonBusEncoders encoders(20,19,21,2);
+CommonBusEncoders encoders(20,19,21,1);
 
 //Default values
-int hsv[3] = {10,10,100};             //HSV array
 int spd = 120;                      //fanspeed
-bool LIGHT;
+bool FANS;
 
 //Setup
-void setup(){
-    FastLED.clear();
-    
+void setup(){  
     //encoders
     encoders.resetChronoAfter(2000);
-    //Encoder 1 cycles through HSV values for FastLED
-    encoders.addEncoder(1,4,18,3,100,0);
-    //Encoder 2 sets the fanspeed in mode 1 and the LED brightness in mode 2
-    encoders.addEncoder(2,4,15,2,200,299);
+    //Encoder 1 on switchpin 18 sets the fanspeed and toggles the fans on/off
+    encoders.addEncoder(1,4,18,1,200,299);
 
     //Fan control
+    FANS = true;
     pwm6configure();
     pwmSet6();
-
-    //LED control
-    FastLED.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS);
-    setColorHSV();
-    LIGHT = true;
 }
 
 //Main loop
@@ -49,45 +32,13 @@ void loop(){
         setValues(code);
     }
     //Fan PWM
-    pwmSet6();
-    //LED control
-    if(LIGHT){setColorHSV();}
+    if(FAN){pwmSet6()};
 }
 
 /*-------------------------------- HELPERS ------------------------------*/
 //Encoder handler
 void setValues(int code){
     switch(code){
-        case 100:{                         //Increase H
-            if(hsv[0] == 255){hsv[0] = 0;}    
-            else{hsv[0]+= 5;}
-            break;
-        }
-        case 101:{                          //Decrease H
-            if(hsv[0] == 0){hsv[0] = 255;}
-            else{hsv[0]-= 5;}
-            break;
-        }
-        case 102:{                          //Increase S
-            if(hsv[1] == 255){hsv[1] = 0;}    
-            else{hsv[1]+= 5;}
-            break;
-        }
-        case 103:{                          //Decrease S
-            if(hsv[1] == 0){hsv[1] = 255;}
-            else{hsv[1]-= 5;}
-            break;
-        }
-        case 104:{                          //Increase V
-            if(hsv[2] == 255){hsv[2] = 0;}    
-            else{hsv[2]+= 5;}
-            break;
-        }
-        case 105:{                          //Decrease V
-            if(hsv[2] == 0){hsv[2] = 255;}
-            else{hsv[2]-= 5;}
-            break;
-        }
         case 200:{                          //Increase fanspeed
             if(spd == 255){}    
             else{spd += 5;}
@@ -99,9 +50,10 @@ void setValues(int code){
             break;
         }
         case 299:{                          //Toggle the LEDs on/off                       
-            LIGHT = !LIGHT;
-            if(!LIGHT){FastLED.clear(true); }
-            else{setColorHSV();}          
+            FANS = !FANS;
+            if(!FANS){spd = 0; }
+            else{spd = 120;}
+            pwmSet6();          
         }
     }
 }
@@ -134,12 +86,3 @@ void pwmSet6()
   TCCR4C |= 0x09;   // Activate channel D
 }
 
-//set HSV
-void setColorHSV() {
-  // create a new HSV color
-  CHSV color = CHSV(hsv[0], hsv[1], hsv[2]);
-
-  // use FastLED to set the color of all LEDs in the strip to the same color
-  fill_solid(leds, NUM_LEDS, color);
-  FastLED.show();
-}
